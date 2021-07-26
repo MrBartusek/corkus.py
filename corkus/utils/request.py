c50561d9eac2aabdf3efc8259a94243b9a159416
MrBartusek <bartusekcraft@gmail.com>
2021-07-27 01:08:30 +0200

    Add user agent

--- corkus/utils/request.py
+++ corkus/utils/request.py
@@ -3,7 +3,10 @@ from corkus.version import __version__
 
 class CorkusRequest:
     def __init__(self, timeout) -> None:
-        self.session = ClientSession(timeout = ClientTimeout(total = timeout))
+        self.session = ClientSession(
+            timeout = ClientTimeout(total = timeout),
+            headers = { "User-Agent": f"Corkus.py/{__version__}" }
+        )
 
     async def get(self, url) -> ClientResponse:
         resp = await self.session.get(url)
