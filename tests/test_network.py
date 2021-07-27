4bc25cf3169102584d99aa6bb6f483f9197e0f4e
MrBartusek <bartusekcraft@gmail.com>
2021-07-27 15:39:01 +0200

    Update test_network.py

--- tests/test_network.py
+++ tests/test_network.py
@@ -5,5 +5,5 @@ class TestNetwork(unittest.IsolatedAsyncioTestCase):
     async def test_player_sum(self):
         corkus = Corkus()
         response = await corkus.network.online_players_sum()
-        self.assertTrue(response.online_players_sum >= 0)
+        self.assertGreaterEqual(response.online_players_sum, 0)
         await corkus.request.session.close()
