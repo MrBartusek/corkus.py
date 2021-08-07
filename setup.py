c4a7910b37fb2f08ea8fe9f06f3f2fe2c52f3869
MrBartusek <bartusekcraft@gmail.com>
2021-08-07 18:16:06 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+import re
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = ''
+with open('corkus/version.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('README.md', encoding="utf8") as f:
+    readme = f.read()
+
+setup(
+    name='corkus.py',
+    author='MrBartusek',
+    url='https://github.com/MrBartusek/corkus.py',
+    project_urls={
+        "Issue tracker": "https://github.com/MrBartusek/corkus.py/issues"
+    },
+    version=version,
+    license='MIT',
+    description='A Python wrapper for the Wynncraft API',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=requirements,
+    python_requires='>=3.8.0',
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Topic :: Internet',
+    'Topic :: Games/Entertainment',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities',
+    ]
+)
