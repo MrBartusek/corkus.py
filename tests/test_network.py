a5f1ec97269465f882388d12a7f813c035b77e0d
MrBartusek <bartusekcraft@gmail.com>
2021-08-10 12:39:16 +0200

    `PlayerStatus` test

--- tests/test_network.py
+++ tests/test_network.py
@@ -21,5 +21,12 @@ class TestNetwork(unittest.IsolatedAsyncioTestCase):
         self.assertTrue(any(s.name.startswith("WC") for s in response))
         self.assertTrue(any(s.type == ServerType.STANDARD for s in response))
 
+    @vcr.use_cassette
+    async def test_online_status(self):
+        response = await self.corkus.network.servers_list()
+        player = await response[0].players[0].fetch()
+        self.assertTrue(player.status.online)
+        self.assertEqual(player.status.server.name, response[0].name)
+
     async def asyncTearDown(self):
         await self.corkus.close()
