1f7cfd3bbd4c7c503c2affe64781a1858f76b163
MrBartusek <bartusekcraft@gmail.com>
2021-08-14 19:31:37 +0200

    Fix pylint

--- corkus/objects/item.py
+++ corkus/objects/item.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Literal, List, Union
+from typing import Union
 
 from .base import CorkusBase
 from .enums import ArmourType, ItemType, ItemTier, ItemRestrictions
