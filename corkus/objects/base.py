2d12bd0b48d6662ca4d0bc71ec0a0661905c9932
MrBartusek <bartusekcraft@gmail.com>
2021-09-08 14:04:12 +0200

    Make `CorkusBase` base `PartialBase`

--- corkus/objects/base.py
+++ corkus/objects/base.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from .partial_base import PartialBase
+
 if TYPE_CHECKING:
     from corkus import Corkus
 
-class CorkusBase:
+class CorkusBase(PartialBase):
     def __init__(self, corkus: Corkus, attributes: dict):
-        self._corkus = corkus
+        super().__init__(corkus)
         self._attributes = attributes
