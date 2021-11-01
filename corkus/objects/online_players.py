20b4b3a91d3ba93f64c2351a38d7c4e96b9487aa
Bartosz Dokurno <bartusekcraft@gmail.com>
2021-11-01 12:31:47 +0100

    Update online_players.py

--- corkus/objects/online_players.py
+++ corkus/objects/online_players.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import List, Union
+from corkus.objects.member import Member
 
 from corkus.utils.utils import Utils
 from .base import CorkusBase
@@ -25,7 +26,7 @@ class OnlinePlayers(CorkusBase):
         """List all online players that are playing on regular servers."""
         return [p for s in self.servers if s.regular for p in s.players]
 
-    def get_player_server(self, player: Union[str, Player, PartialPlayer, PartialOnlinePlayer]) -> Union[Server, None]:
+    def get_player_server(self, player: Union[str, Player, PartialPlayer, PartialOnlinePlayer, Member]) -> Union[Server, None]:
         """Get the server that player is currently playing on. Returns ``None`` if player is offline.
 
         :param player: Player username or player object to check for.
